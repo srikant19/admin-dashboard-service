4ad16250f43ac74d74eaa5f0362d62ab17ef5642
srikant19 <srikant.suman19@gmail.com>
2025-02-26 00:17:38 +0530

    vendi: admin dashboard service

---
+++ admindashboardservice/src/main/java/com/vendi/admindashboardservice/AdmindashboardserviceApplication.java
@@ -0,0 +1,13 @@
+package com.vendi.admindashboardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmindashboardserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmindashboardserviceApplication.class, args);
+	}
+
+}
---
+++ admindashboardservice/src/test/java/com/vendi/admindashboardservice/AdmindashboardserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vendi.admindashboardservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmindashboardserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
